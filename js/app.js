8bd2a2d430991d7b83192ad6e59b33011dbca15c
Ethan McGregor <ermmcgregor@gmail.com>
2016-12-04 14:20:33 -0800

    added finsished code from the tutorial

--- js/app.js
+++ js/app.js
@@ -1,1 +1,13 @@
 'strict'
+google.charts.load("current", { packages: ["corechart"] });
+google.charts.setOnLoadCallback(drawChart);
+
+function drawChart() {
+    var data = google.visualization.arrayToDataTable([['Task', 'Hours per day'], ['Work', 11], ['eat', 2], ['Commute', 2], ['Watch Tv', 1.5]]);
+
+    var options = { title: 'My Daily Activities', is3D:true }
+
+    var chart = new google.visualization.Histogram(document.getElementById('piechart'));
+    chart.draw(data, options);
+};
+
